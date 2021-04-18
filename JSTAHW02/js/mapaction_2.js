e0a6ad9fdee5960ce852d4ddee8ae7c73e2316fb
JF <tksymio@gmail.com>
2021-04-18 23:27:46 +0800

    extract inline  MarkClick event

--- JSTAHW02/js/mapaction_2.js
+++ JSTAHW02/js/mapaction_2.js
@@ -139,11 +139,7 @@ function AddMarkerWithInfo(position, icon,title,infoContent) {
     // create infoWindow
     let info = new google.maps.InfoWindow({ content: infoContent });
 
-    // add marker infowindow
-    marker.addListener("click", () => {
-        ClearInfo();
-        info.open(map, marker)
-    });
+    marker.addListener("click", MarkerClick);
 
     // add marker click event
     // marker.addListener("click", toogleBounce);
@@ -159,7 +155,12 @@ function AddMarkerWithInfo(position, icon,title,infoContent) {
     infos.push(info);
     markers.push(marker);
 }
-
+function MarkerClick(){
+    ClearInfo();
+    this.map.center = this.position,
+    this.map.zoom = 17;
+    info.open(map, marker)
+}
 function DeleteMarkers() {
 
     markers.forEach(item => {
