03d9be6f9479d788c481a87e9c253769d89ee13b
JF <tksymio@gmail.com>
2021-04-25 20:18:32 +0800

    modify  create toDoItem function

--- JSTAHW03/js/action-03.js
+++ JSTAHW03/js/action-03.js
@@ -1,4 +1,5 @@
 let todayDate, currentDate, currentCalendarData, schedules;
+const daytoDoBoxResizeObs = new ResizeObserver(daytoDoBoxResizeAction);
 const monuthNames = [
     "January",
     "February",
@@ -64,7 +65,7 @@ schedules = [
         date: 20,
         hour: 01,
         minute: 02,
-        title: "12311111111",
+        title: "04200102",
         description: "----"
     },
     {
@@ -219,15 +220,36 @@ window.onload = function () {
     // console.log(todayDate);
     // console.log(currentCalendarData);
 
+    // create resizeObserve for calendar-daysWrap
+    let daysWrap = document.querySelector(".calendar-daysWrap");
+    // daysWrapResizeObserver.observe(daysWrap);
+
 
     // register btnPrev click event
     document.querySelector(".calendar-btnPrev").addEventListener("click", function () { IncreaseCalendar(false) });
 
     // register btnNext click event
     document.querySelector(".calendar-btnNext").addEventListener("click", function () { IncreaseCalendar(true) });
+   
+   
+   
     // document.querySelector(".addEvent").addEventListener("click", function () { console.dir(document.querySelector("#eventTimeInput").value) });
 
+    // document.querySelector(".calendar-date").addEventListener("click",function(){
+
+    //     let daysInfo = document.querySelectorAll(".dayInfo-toDoBox");
+
+    //     daysInfo.forEach((item)=>{
+    //         daytoDoBoxResizeObs.observe(item);
+    //     });
+    // });
+
+
+
 };
+
+
+
 
 function InitAddToDoModal() {
     let date = new Date();
@@ -332,9 +354,7 @@ function YM_Equal(dateA, dateB) {
 function ApplyCalendarDays(calendarData, isIncrease) {
     let calendar = document.querySelector(".calendar");
 
-    calendar.querySelector(
-        ".calendar-date :first-child"
-    ).innerText = calendarData.title.year;
+    calendar.querySelector(".calendar-date :first-child").innerText = calendarData.title.year;
     calendar.querySelector(".calendar-date :last-child").innerText =
         monuthNames[calendarData.title.month];
 
@@ -345,8 +365,6 @@ function ApplyCalendarDays(calendarData, isIncrease) {
     let calendarDays = CreateCalendardays(calendarData);
     calendarDays.style.opacity = 0;
     calendardaysWrap.appendChild(calendarDays);
-
-
     AddDaysToDoItem(calendarDays, calendarData);
 
     // calendarDays is not exist, add new calendardays
@@ -365,8 +383,6 @@ function ApplyCalendarDays(calendarData, isIncrease) {
             existCalendarDays.style.cssText =
                 "opacity:0; transform:translate(100%,0); transition: .2s;";
             calendarDays.style.cssText = "opacity:1; transform:translate(-100%,0);";
-            console.log(calendarDays.clientHeight);
-            console.log(calendarDays.clientWidth);
         }
 
         setTimeout(() => {
@@ -380,17 +396,6 @@ function ApplyCalendarDays(calendarData, isIncrease) {
     }
 }
 
-function CreateDayInfotoDoItem(scheduleItem) {
-    let clonetent = document.querySelector("#tpl-dayInfo-toToItem").content.cloneNode(true);
-    clonetent.querySelector("span").innerText = `${scheduleItem.hour.toString().padStart(2,"0")}:${scheduleItem.minute.toString().padStart(2,"0")} ${scheduleItem.title}`;
-    clonetent.querySelector("button").addEventListener("click",function(event){
-        event.stopPropagation();
-        console.log(`this is ${scheduleItem.hour.toString().padStart(2,"0")}:${scheduleItem.minute.toString().padStart(2,"0")} ${scheduleItem.title}`)
-    });
-
-
-    return clonetent;
-}
 
 
 // Create DOM of Calendardays
@@ -406,7 +411,7 @@ function CreateCalendardays(calendarData) {
         // let daytitle = clonedayInfo.querySelector(".dayTitle");
 
         let istodayDate = YMD_Equal(item.toDateObj(), todayDate);
-        dayinfo.addEventListener("click",function(){console.log("dayInfo is clicked")});
+        dayinfo.addEventListener("click", function () { console.log("dayInfo is clicked") });
         dayinfo.querySelector(".dayInfo-title").innerText = `${monuthNames[item.month]} ${item.date}`;
         if (istodayDate) {
             dayinfo.classList.add("currentDay");
@@ -435,6 +440,7 @@ function CreateCalendardays(calendarData) {
                 } ${item.date}`;
             dayinfo.classList.add("outtarget");
         }
+
         calendarDays.appendChild(clonedayInfo);
     });
 
@@ -445,16 +451,94 @@ function CreateCalendardays(calendarData) {
 // 
 function AddDaysToDoItem(target, calendarData) {
 
+    // clear previous resizeobserve
+    daytoDoBoxResizeObs.disconnect();
 
-    let daysInfo = target.querySelectorAll(".dayInfo-toDoBox");
+    let toDoBoxes = target.querySelectorAll(".dayInfo-toDoBox");
 
-    calendarData.dates.forEach((date, index) => {
+    toDoBoxes.forEach((tDoBox) => {
+        daytoDoBoxResizeObs.observe(tDoBox);
+    });
 
-        date.toDoList.forEach((item) => {
-            daysInfo[index].appendChild(CreateDayInfotoDoItem(item));
-        });
+    // calendarData.dates.forEach((date, index) => {
+    //     date.toDoList.forEach((item) => {
+    //         daysInfo[index].appendChild(CreateDayInfotoDoItem(item));
+    //     });
+    //     // daytoDoBoxResizeObs.observe(daysInfo[index]);
+    // });
+}
+
+function daytoDoBoxResizeAction(entries) {
+    entries.forEach((entry, index) => {
+        let toDoList = currentCalendarData.dates[index].toDoList;
+        CreateDayInfotoDoItem(entry, toDoList);
     });
 }
+
+function CreateDayInfotoDoItem(resizeEntry, toDoList) {
+    let contentHeight, totaltoDoListHeight, toDoItem;
+    const toDoItemHeight = 22;
+    const moreInfoHeight = 22;
+    if (toDoList.length !== 0) {
+
+        contentHeight = resizeEntry.contentBoxSize[0].blockSize;
+        // assume the height of each toDoItem is 22px
+        totaltoDoListHeight = toDoList.length * toDoItemHeight;
+        // clear exist toDoItem
+        resizeEntry.target.innerHTML = "";
+
+        // 
+        if (totaltoDoListHeight <= contentHeight) {
+
+            toDoList.forEach((scheduleItem) => {
+                toDoItem = CreateToDoItem(scheduleItem.hour, scheduleItem.minute, scheduleItem.title);
+                resizeEntry.target.appendChild(toDoItem);
+            });
+        }
+        else {
+
+            contentHeight -= moreInfoHeight;
+            let index = 0;
+
+            // create todoItem until the space is used up
+            while ((contentHeight -= toDoItemHeight) > 0) {
+                toDoItem = CreateToDoItem(toDoList[index].hour, toDoList[index].minute, toDoList[index].title);                
+                resizeEntry.target.appendChild(toDoItem);
+                index++
+            }
+
+            // if space is not enough space, use the text of button to indicate
+            let remain = document.createElement("button");
+            remain.classList.add("dayInfo-toDoItemMore")
+            remain.innerText = `${toDoList.length - index} more `;
+            resizeEntry.target.appendChild(remain);
+        }
+    }
+}
+
+function CreateToDoItem(hour, minute, toDoTitle) {
+    let clonetent = document.querySelector("#tpl-dayInfo-toToItem").content.cloneNode(true);
+    clonetent.querySelector("span").innerText = `${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")} ${toDoTitle}`;
+    return clonetent;
+}
+
+
+// function CreateDayInfotoDoItem(scheduleItem) {
+//     let clonetent = document.querySelector("#tpl-dayInfo-toToItem").content.cloneNode(true);
+//     clonetent.querySelector("span").innerText = `${scheduleItem.hour.toString().padStart(2, "0")}:${scheduleItem.minute.toString().padStart(2, "0")} ${scheduleItem.title}`;
+//     clonetent.querySelector("button").addEventListener("click", function (event) {
+//         event.stopPropagation();
+//         // console.log(`this is ${scheduleItem.hour.toString().padStart(2,"0")}:${scheduleItem.minute.toString().padStart(2,"0")} ${scheduleItem.title}`)
+//         this.parentNode.removeChild(this);
+//     });
+
+
+//     return clonetent;
+// }
+
+
+
+
 
 // Create UUID
 // reference : pdf file of calendar  from TA
@@ -479,31 +563,3 @@ function StringCutOff(input, maxLength) {
         return input.substring(0, maxLength - 3) + "...";
     }
 }
-
-
-
-
-  // console.log(item.toDoList);
-
-
-
-    // ............................................................
-
-
-    // let dayInfoToDoBox = dayinfo.querySelector(".dayInfo-toDoBox");
-    // item.toDoList.forEach((todo) => {
-
-    //   let toDoItem = document.createElement("div");
-    //   toDoItem.classList.add("toDoItem");
-
-
-
-    //   // toDoItem.innerText = todo.title;
-
-
-    //   // StringCutOff(string, maxlength)
-
-
-    //   dayInfoToDoBox.appendChild(toDoItem);
-
-    // });
